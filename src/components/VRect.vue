4d6022b43d785eea8d6138a9955682c4e608e924
Soichiro <soichiro@SoichironoMacBook-Pro.local>
2019-10-25 17:40:27 +0900

    first commit

---
+++ src/components/VRect.vue
@@ -0,0 +1,36 @@
+<template>
+  <g>
+    <rect
+      :x="x" :y="y"
+      rx="3" ry="3"
+      width="60.515625" height="23"
+      stroke="black" stroke-width="1"
+      fill="#B3E5FC" fill-opacity="1"
+      stroke-dasharray="0" stroke-dashoffset="0"
+    />
+    <text
+      :x="x" :y="y"
+      font-size="10" font-weight="500"
+      fill="#000000"
+      text-decoration
+    >{{text}}</text>
+  </g>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+    }
+  },
+  props: {
+    text: String,
+    x: Number,
+    y: Number,
+  },
+  computed: {
+  },
+  mounted: function() {
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 640 640"
+      width="640"
+      height="640"
+    >
+      <rect width="200" height="80" x="100" y="10" rx="10" fill="black"/>
+      <rect width="200" height="80" x="100" y="100" rx="10" fill="black"/>
+      <g>
+        <rect x="0" y="0" width="100" height="100" fill="red"></rect>
+        <text x="0" y="50" font-family="Verdana" font-size="35" fill="blue">Hello</text>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {}
+};
+</script>
